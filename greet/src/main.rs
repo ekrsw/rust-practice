4e799ab79bedb3798c144c0fb1ad65a9bcc56fd2
ekrsw <eisuke.koresawa@gmail.com>
2026-01-15 12:24:24 +0900

    M

--- greet/src/main.rs
+++ greet/src/main.rs
@@ -1,12 +1,24 @@
+use std::io;
 use std::env;
 
-fn main() -> Result<(), String>{
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        return Err("Usage: greet <name>".into());
+fn main() {
+    let args = env::args().skip(1);
+
+    let name = if let Some(name) = args.next() {
+        name
     } else {
-        let name = &args[1];
-        println!("Hello, {}!", name);
-        Ok(())
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read from stdin");
+        
+        input.trim().to_string()
+    };
+
+    if name.is_empty() {
+        eprintln!("Name is empty.");
+        std::process::exit(1);
     }
+
+    println!("Hello, {}!", name);
 }
